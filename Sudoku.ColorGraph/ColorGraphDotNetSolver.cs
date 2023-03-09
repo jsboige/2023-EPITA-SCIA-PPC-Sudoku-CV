a8e78aa0cb38ac95e9cb020d300bcfd3eb1d110e
Louis <louis14.pagnier@gmail.com>
2023-03-09 12:32:05 +0100

    Template Coloration Graph

---
+++ Sudoku.ColorGraph/ColorGraphDotNetSolver.cs
@@ -0,0 +1,13 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.ColorGraph
+{
+    public class ColorGraphDotNetSolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
