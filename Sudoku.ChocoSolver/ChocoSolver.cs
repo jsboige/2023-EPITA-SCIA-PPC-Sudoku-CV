9f2ecc342f9b304b69534b7b3a76c06a08281781
raphrb2 <raphael.bennaim@epita.fr>
2023-03-15 16:47:08 +0100

    added libraries to project

--- Sudoku.ChocoSolver/ChocoSolver.cs
+++ Sudoku.ChocoSolver/ChocoSolver.cs
@@ -1,4 +1,12 @@
-﻿namespace Sudoku.ChocoSolver{
+﻿using Python.Runtime;
+using Sudoku.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.ChocoSolver{
     public class ChocoSolver
     {
         
