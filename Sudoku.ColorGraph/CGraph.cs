3840b1c8afcd1df8f2cbeaafdfbb55877b22ffa4
Zspirit23 <sronleo@gmail.com>
2023-03-15 17:47:50 +0100

    First draft Dsatur algorithm

--- Sudoku.ColorGraph/CGraph.cs
+++ Sudoku.ColorGraph/CGraph.cs
@@ -3,24 +3,47 @@ using Sudoku.Shared;
 
 namespace Sudoku.ColorGraph
 {
+    
+    public class Pair
+    {
+        public int Item1;
+        public int Item2;
+        public Pair(int item1, int item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+       
+    }
+    
     public class CGraph
     {
-        private List<List<(int, int)>> AdjacencyList;
-
+        public List<List<Pair>> AdjacencyList;
+        public List<Pair> nodes;
         public CGraph(SudokuGrid s)
         {
-            AdjacencyList = new List<List<(int, int)>>();
-            for (int i = 0; i <= 9; i++)
+            nodes = new List<Pair>();
+            for (int i = 0; i < 81 ; i++)
+                nodes.Add(new Pair(i, 0));
+            
+            AdjacencyList = new List<List<Pair>>();
+            for (int i = 0; i < 9; i++)
             {
-                for (int j = 0; j <= 9; j++)
+                for (int j = 0; j < 9; j++)
                 {
-                    HashSet<(int, int)> subList = new HashSet<(int, int)>();
+                    HashSet<Pair> subList = new HashSet<Pair>();
                     for (int cell = 0; cell < 9; cell++)
                     {
                         if (cell != j)
-                            subList.Add((i * 9 + cell, s.Cells[i][cell]));
+                        {
+                            nodes[i * 9 + cell].Item2 = s.Cells[i][cell];
+                            subList.Add(nodes[i * 9 + cell]);
+                        }
                         if (cell != i)
-                            subList.Add((cell * 9 + j, s.Cells[cell][j]));
+                        {
+                            nodes[cell * 9 + j].Item2 = s.Cells[cell][j];
+                            subList.Add(nodes[cell * 9 + j]);
+                        }
                     }
                     // insert in the hashset the cells in the same block
                     int iBlock = i / 3;
@@ -28,7 +51,10 @@ namespace Sudoku.ColorGraph
                     for (int a = 0; a < 3; a++)
                         for (int b = 0; b < 3; b++)
                             if (!(iBlock * 3 + a == i && jBlock * 3 + b == j))
-                                subList.Add(((iBlock * 3 + a) * 9 + jBlock * 3 + b, s.Cells[iBlock * 3 + a][jBlock * 3 + b]));
+                            {
+                                nodes[(iBlock * 3 + a) * 9 + jBlock * 3 + b].Item2 = s.Cells[iBlock * 3 + a][jBlock * 3 + b];
+                                subList.Add(nodes[(iBlock * 3 + a) * 9 + jBlock * 3 + b]);
+                            }
 
                     AdjacencyList.Add(subList.ToList());
                 }
--- Sudoku.ColorGraph/ColorGraphDotNetSolver.cs
+++ Sudoku.ColorGraph/ColorGraphDotNetSolver.cs
@@ -6,9 +6,125 @@ namespace Sudoku.ColorGraph
     {
         public SudokuGrid Solve(SudokuGrid s)
         {
-            new CGraph(s);
-	        return s;
+            return Dsatur(new CGraph(s));
         }
+        
+        private HashSet<List<Pair>> GetUncoloredVertices(CGraph g)
+        {
+            var uncoloredVertices = new HashSet<List<Pair>>();
+            foreach (var vertex in g.nodes)
+            {
+                if (vertex.Item2 == 0)
+                {
+                    uncoloredVertices.Add(g.AdjacencyList[vertex.Item1]);
+                }
+            }
+            
+            return uncoloredVertices;
+        }
+        
+        // Solve Color Graph using Dsatur algorithm
+        private SudokuGrid Dsatur(CGraph g)
+        {
+            var uncoloredVertices = GetUncoloredVertices(g);
+            int cpt = 0;
+            while (uncoloredVertices.Count > 0)
+            {
+                var maxSaturation = 0;
+                var maxSaturationVertex = new List<Pair>();
+                foreach (var vertex in uncoloredVertices)
+                {
+                    var saturation = GetSaturation(vertex);
+                    if (saturation > maxSaturation)
+                    {
+                        maxSaturation = saturation;
+                        maxSaturationVertex = vertex;
+                    }
+                }
+                
+                var maxDegree = 0;
+                var maxDegreeVertex = new Pair(0, 0);
+                foreach (var vertex in maxSaturationVertex)
+                {
+                    var degree = GetDegree(vertex, g);
+                    if (degree > maxDegree)
+                    {
+                        maxDegree = degree;
+                        maxDegreeVertex = vertex;
+                    }
+                }
+                
+                var color = 1;
+                var colors = new HashSet<int>();
+                foreach (var vertex in maxSaturationVertex)
+                {
+                    if (vertex.Item2 != 0)
+                    {
+                        colors.Add(vertex.Item2);
+                    }
+                }
+                
+                while (colors.Contains(color))
+                {
+                    color++;
+                }
+                
+                maxDegreeVertex.Item2 = color;
+                
+                uncoloredVertices = GetUncoloredVertices(g);
+                // print length of the uncolored vertices every 10000 iterations
+                if (cpt % 10 == 0)
+                {
+                    System.Console.WriteLine(uncoloredVertices.Count);
+                }
+
+                cpt++;
+            }
+            
+            var result = new SudokuGrid();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    result.Cells[i][j] = g.nodes[i * 9 + j].Item2;
+                }
+                
+            }
+            
+            
+            
+            return result;
+        }
+        
+        private int GetDegree(Pair vertex, CGraph g)
+        {
+            var degree = 0;
+            foreach (var v in g.AdjacencyList[vertex.Item1])
+            {
+                if (v.Item2 != 0)
+                {
+                    degree++;
+                }
+            }
+            
+            return degree;
+        }
+        
+        private int GetSaturation(List<Pair> vertex)
+        {
+            var saturation = 0;
+            foreach (var v in vertex)
+            {
+                if (v.Item2 != 0)
+                {
+                    saturation++;
+                }
+            }
+            
+            return saturation;
+        }
+
+        
     }
 }
 
