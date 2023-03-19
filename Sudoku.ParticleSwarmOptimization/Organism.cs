4fb9dce3c7924098fa6cf1c7bcd5ebf5f79584cd
raphael.mourot-pelade <raphael.mourot-pelade@epita.fr>
2023-03-19 19:30:15 +0100

    fill the grid with for correct columns

--- Sudoku.ParticleSwarmOptimization/Organism.cs
+++ Sudoku.ParticleSwarmOptimization/Organism.cs
@@ -21,7 +21,26 @@ namespace Sudoku.ParticleSwarmOptimization
             solution = s.CloneSudoku();
             
             // Fill the Sudoku grid with random values but repecting column constraint
+            Random r = new Random();
+            for (int i = 0; i < 9; ++i)
+            {
+                int[] possibleNumbers = SudokuGridHelpers.AvailableNumbersForColumn(solution, i);
+                for (int j = 0; j < 9; ++j)
+                {
+                    if (solution.Cells[i][j] != 0)
+                    {
+                        continue;
+                    }
 
+                    int randomIndex = 0;
+                    do
+                    {
+                        randomIndex =  r.Next(0, possibleNumbers.Length);
+                    } while (possibleNumbers[randomIndex] == 0);
+                    solution.Cells[i][j] = possibleNumbers[randomIndex];
+                    possibleNumbers[randomIndex] = 0;
+                }
+            }
             error = solution.NbErrors(s);
             Console.WriteLine($"Error: {Error} worker? {is_worker}");
         }
