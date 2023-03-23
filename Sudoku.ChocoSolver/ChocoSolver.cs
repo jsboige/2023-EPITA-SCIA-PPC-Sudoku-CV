2540c505cd5cba42fd208bc3164cc6059eaa24f1
Tom Genlis <bryckout@gmail.com>
2023-03-23 12:54:29 +0100

    fix(solver): using Py.GIL to prevent memory errors

--- Sudoku.ChocoSolver/ChocoSolver.cs
+++ Sudoku.ChocoSolver/ChocoSolver.cs
@@ -18,43 +18,40 @@ namespace Sudoku.Demo
             //System.Diagnostics.Debugger.Break();
 
             //For some reason, the Benchmark runner won't manage to get the mutex whereas individual execution doesn't cause issues
-            //using (Py.GIL())
-            //{
-            // create a Python scope
-            using (PyModule scope = Py.CreateScope())
+            using (Py.GIL())
             {
-                AddNumpyConverterScript(scope);
-                // convert the Cells array object to a PyObject
-                PyObject pyCells = s.Cells.ToPython();
+                // create a Python scope
+                using (PyModule scope = Py.CreateScope())
+                {
+                    // convert the Cells array object to a PyObject
+                    PyObject pyCells = s.Cells.ToPython();
 
-                // create a Python variable "instance"
-                scope.Set("instance", pyCells);
+                    // create a Python variable "instance"
+                    scope.Set("instance", pyCells);
 
-                //this.AddNumpyConverterScript(scope);
+                    //this.AddNumpyConverterScript(scope);
 
-                // run the Python script
-                string code = Resource1.solver_py;
-                scope.Exec(code);
+                    // run the Python script
+                    string code = Resource1.solver_py;
+                    scope.Exec(code);
 
+                    //Retrieve solved Sudoku variable
+                    var result = scope.Get("r");
 
-                //Retrieve solved Sudoku variable
-                var result = scope.Get("r");
-
-                //Convert back to C# object
-                var managedResult = result.As<int[][]>();
-                //var convertesdResult = managedResult.Select(objList => objList.Select(o => (int)o).ToArray()).ToArray();
-                return new Shared.SudokuGrid() { Cells = managedResult };
+                    //Convert back to C# object
+                    var managedResult = result.As<int[][]>();
+                    //var convertesdResult = managedResult.Select(objList => objList.Select(o => (int)o).ToArray()).ToArray();
+                    return new Shared.SudokuGrid() { Cells = managedResult };
+                }
             }
-            //}
-
         }
-
+        
         protected override void InitializePythonComponents()
         {
             //declare your pip packages here
             InstallPipModule("numpy");
+            InstallPipModule("pycsp3");
             base.InitializePythonComponents();
         }
-
     }
 }
