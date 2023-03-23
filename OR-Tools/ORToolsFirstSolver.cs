c028bc283164a75a7f3dc4acc391b090c13ff2f0
Bryan Chen <bryan.chen@epita.fr>
2023-03-23 12:27:37 +0100

    Add the new project for the solver

---
+++ OR-Tools/ORToolsFirstSolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Google.OrTools.ConstraintSolver;
+using Google.OrTools.Sat;
+using Sudoku.Shared;
+
+namespace Sudoku.OR.Tools;
+
+public class ORToolsFirstSolver : Sudoku.Shared.ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        CpModel model = new CpModel();
+        return s;
+    } 
+}
